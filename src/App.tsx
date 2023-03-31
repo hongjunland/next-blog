f13ad696684826c4c00f5d004552c4a5021f3a89
hongjunland <zunza96@gmail.com>
2023-03-31 16:37:58 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        njbjbbnn
         <a
           className="App-link"
           href="https://reactjs.org"
