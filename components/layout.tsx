b4bd6c242d9e3c8f8d2e6e8df47e66aa65f29597
이홍준 <zunza96@naver.com>
2023-04-04 22:12:52 +0900

    update

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,11 @@ import Link from 'next/link';
 const name = 'Honjun Lee';
 export const siteTitle = 'Next.js Sample Website';
 
-export default function Layout({ children, home }) {
+interface Props{
+  children: React.ReactNode;
+  home: boolean;
+}
+export default function Layout({ children, home }: Props) {
   return (
     <div className={styles.container}>
       <Head>
